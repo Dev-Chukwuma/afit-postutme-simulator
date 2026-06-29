c1f0acf6d888198292584c838b780ecb8f183c2f
Dev-Chukwuma <divinemc13@gmail.com>
2026-06-29 12:04:15 +0100

    fix secret key and hosting config

--- afit_postutme/settings.py
+++ afit_postutme/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-temporary-key-for-local')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default = False, cast=bool)
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+from decouple import config, Csv
 import os
 
 MEDIA_URL = '/media/'
